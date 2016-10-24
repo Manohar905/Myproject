45a293d05605d543c8ee38af6f232559612b5f32
satya sai manohar koduru <satya sai manohar koduru>
2016-10-23 18:11:33 -0700

    First commit

---
+++ Hello.java
@@ -0,0 +1,18 @@
+package com.manohar.typecasting;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		//int length=10,breadth=20;
+		//int area=length * breadth;
+
+double a=100.0;
+int b=(int)a;
+		
+		
+		System.out.println(b);
+		// TODO Auto-generated method stub
+
+	}
+
+}
